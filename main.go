a16a8102448b88e5663b468d584268ca11e58960
Melbeck777 <molmin729@gmail.com>
2026-01-17 15:18:00 +0900

    [add] use cli.

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 /*
 Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/Melbeck777/noq.git/cmd"
+import (
+	"os"
+
+	"github.com/Melbeck777/noq/internal/adapter/cli"
+)
 
 func main() {
-	cmd.Execute()
+	if err := cli.NewRootCmd().Execute(); err != nil {
+		os.Exit(1)
+	}
 }
