05375832b11298d969ce75fddaf37caa95f67e33
Melbeck777 <molmin729@gmail.com>
2026-02-02 20:42:57 +0900

    fix: SaveOverwrite to SaveOverWrite

--- internal/application/repository/ConfigRepository.go
+++ internal/application/repository/ConfigRepository.go
@@ -5,5 +5,5 @@ import "github.com/Melbeck777/noq/internal/domain/entity"
 type ConfigRepository interface {
 	Load() (entity.Config, error)
 	Save(entity.Config) error
-	SaveOverwrite(entity.Config) error
+	SaveOverWrite(entity.Config) error
 }
