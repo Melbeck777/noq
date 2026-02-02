589d220553555b22808fccdceeca820801ffe2ef
Melbeck777 <molmin729@gmail.com>
2026-02-02 20:43:31 +0900

    add: NotionConfig Entity

--- internal/domain/entity/NotionConfig.go
+++ internal/domain/entity/NotionConfig.go
@@ -10,3 +10,22 @@ type NotionConfig struct {
 	DefaultMemoDB valueobject.DatabaseAlias
 	Databases     valueobject.NotionDatabases
 }
+
+func NewNotionConfig(token, defaultMemoDb string, databases map[string]string) *NotionConfig {
+	dbAlias, _ := valueobject.NewDatabaseAlias(defaultMemoDb)
+	dbs, _ := valueobject.NewNotionDatabases(databases)
+	return &NotionConfig{
+		Token:         token,
+		DefaultMemoDB: *dbAlias,
+		Databases:     dbs,
+	}
+}
+
+func (n *NotionConfig) UpdateDefaultMemoDB(memo string) error {
+	update_value, err := valueobject.NewDatabaseAlias(memo)
+	if err != nil {
+		return err
+	}
+	n.DefaultMemoDB = *update_value
+	return nil
+}
