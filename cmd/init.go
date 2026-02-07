6015ca9ec2bd50de64a770a4fd6a4591650680fe
Melbeck777 <molmin729@gmail.com>
2026-02-07 12:51:17 +0900

    add: cmd/init for making initial setup

--- cmd/init.go
+++ cmd/init.go
@@ -21,10 +21,6 @@ func newInitCmd(ui *rwi.RWI, configUseCase usecase.ConfigUseCase) *cobra.Command
 		Short: "init memo and article",
 		Long:  `setting the your memo_root, article_root and default_memo_db`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// 入力を受け取るようにする
-			// 受け取った入力をentityに押し込める
-			// request dtoが必要でこれは標準の型でしゅつりょくするような形式とする
-			// 入力を受付の終了としてqがおされたら入力を終了して次に進む
 			currentConfig, err := configUseCase.Load()
 			if err != nil {
 				tmp, err := configUseCase.DefaultValue()
@@ -41,13 +37,32 @@ func newInitCmd(ui *rwi.RWI, configUseCase usecase.ConfigUseCase) *cobra.Command
 			inputs := []prompt.Input{
 				{Title: "memo_root", Value: &now.MemoRoot},
 				{Title: "article_root", Value: &now.ArticleRoot},
+				{Title: "notion_token", Value: &now.NotionToken},
+				{Title: "default_memo_db", Value: &now.DefaultMemoDB},
 			}
 			if err := prompt.UpdateInputs(inputs); err != nil {
 				return err
 			}
-			fmt.Printf("%s, %s", now.MemoRoot, now.ArticleRoot)
 
-			return nil
+			MapInput := prompt.MapInput{
+				KeyDescritpiton:   "Notion Database key > ",
+				ValueDescritpiton: "Notion Database Id > ",
+				QuitMark:          "q",
+				Map:               now.Databases,
+			}
+			// Mapの初期化を入れる入れるならmapを定義する構造体もしくはマップを定義する際に利用するより共通の構造体
+			// アーキとしてコモン的なものを作るのはありかどうか
+			prompt.MapInputs(MapInput)
+
+			// prettyPrint(now.Databases) // This line is now redundant
+			cfg, err := Mapper.InitRequestDtoToConfig(now)
+			fmt.Println(cfg, err)
+			if err != nil {
+				fmt.Println(err)
+				return err
+			}
+
+			return configUseCase.SaveOverWrite(cfg)
 		},
 	}
 	return initCmd
